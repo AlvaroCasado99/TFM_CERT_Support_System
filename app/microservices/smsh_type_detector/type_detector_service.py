c0a92d54b0393fbc64a5474906c96f1edee05ab4
AlvaroCasado99 <acasam01@estudiantes.unileon.es>
2025-07-10 16:10:31 +0000

    Versiones Funcionales para SimpleTransformers. Código para el servicio de tipos de smishing hecho.

--- app/microservices/smsh_type_detector/type_detector_service.py
+++ app/microservices/smsh_type_detector/type_detector_service.py
@@ -1,15 +1,17 @@
 import os
-# import torch
+import torch
+import json
 
 from pydantic import BaseModel
 from fastapi import FastAPI
-# from simpletransformers.classification import ClassificationModel
+from simpletransformers.classification import ClassificationModel
 
 # API
 app = FastAPI()
 
 # Cargar el modelo
 model = None
+classes = None
 
 # Modelo para el cuerpo de los requests
 class Request(BaseModel):
@@ -19,16 +21,36 @@ class Request(BaseModel):
 @app.on_event("startup")
 def app_init():
     global model
-    # use_cuda = torch.cuda.is_available()
-    # model = ClassificationModel('bert', 'ruta/al/modelo/guardado', use_cuda=use_cuda)
+    global classes
+
+    model_path = '/home/vboxuser/Downloads/BERT_170625_223642_7_CLASSES'
+
+    use_cuda = torch.cuda.is_available()
+    model = ClassificationModel('bert', model_path, use_cuda=use_cuda)
+
+    # Obtener el mapa de clases
+    with open(f"{model_path}/training_args.json", "r") as f:
+        classes = json.load(f)['labels_map']
+    classes = {v: k for k, v in classes.items()}    # Invertir el orden
 
 # Endpoint que comprueba el mensaje
 @app.post("/check")
 def smhs_type(req: Request):
+    print("Dentro")
+
     msg = [req.msg]
 
-    # predictions, raw_outputs = model.predict(msg)
+    print("A ver que dice...")
+
+    predictions, raw_outputs = model.predict(msg)
+
+    # Convertir tensores a listas
+    # raw_outputs = [output.tolist() for output in raw_outputs]
+
+    # Sacar la predicción y convertirla a texto con el mapa de clases
+    predictions = classes[predictions.tolist()[0]]
 
     return {
-            "predictions": "Indefinido"
+            "predictions": predictions
+            # "outputs": raw_outputs
             }
